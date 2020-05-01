a460caab1e062c792e34edc9626a7bb787744a18
Samantha Rand <samantharand@Samanthas-MacBook-Pro-2.local>
2020-05-01 10:40:19 -0500

    login/register page has conditional rendering

--- src/LoginRegisterForm/index.js
+++ src/LoginRegisterForm/index.js
@@ -15,6 +15,18 @@ export default class LoginRegisterForm extends Component {
 		}
 	}
 
+	switchAction = () => {
+		if(this.state.action == "Login")  {
+			this.setState({
+				action: 'Register'
+			})
+		} else {
+			this.setState({
+				action: 'Login'
+			})
+		}
+	}
+
 	handleChange = (event) => {
 		this.setState({
 			[event.target.name]: event.target.value
@@ -40,14 +52,20 @@ export default class LoginRegisterForm extends Component {
 						placeholder='username :)'
 						onChange={this.handleChange}
 					/>
-					<Label> email </Label>
-					<Form.Input 
-						name='email'
-						type='text'
-						value={this.state.value}
-						placeholder='email :)'
-						onChange={this.handleChange}
-					/>
+					{
+						this.state.action == 'Register'
+						&&
+						<React.Fragment>
+							<Label> email </Label>
+							<Form.Input 
+								name='email'
+								type='text'
+								value={this.state.value}
+								placeholder='email :)'
+								onChange={this.handleChange}
+							/>
+						</React.Fragment>
+					}
 					<Label> password </Label>
 					<Form.Input 
 						name='password'
@@ -56,33 +74,46 @@ export default class LoginRegisterForm extends Component {
 						placeholder='password :)'
 						onChange={this.handleChange}
 					/>
-					<h3> fun stuff </h3>
-					<Label> age </Label>
-					<Form.Input 
-						name='age'
-						type='number'
-						value={this.state.value}
-						placeholder='age :)'
-						onChange={this.handleChange}
-					/>
-					<Label> location </Label>
-					<Form.Input 
-						name='location'
-						type='text'
-						value={this.state.value}
-						placeholder='location :)'
-						onChange={this.handleChange}
-					/>
-					<Label> bio </Label>
-					<Form.TextArea 
-						name='bio'
-						type='text'
-						value={this.state.value}
-						placeholder='bio :)'
-						onChange={this.handleChange}
-					/>
-					<Button type='submit'>create account</Button>
+					{
+						this.state.action == 'Register'
+						&&
+						<React.Fragment>
+							<h3> fun stuff </h3>
+							<Label> age </Label>
+							<Form.Input 
+								name='age'
+								type='number'
+								value={this.state.value}
+								placeholder='age :)'
+								onChange={this.handleChange}
+							/>
+							<Label> location </Label>
+							<Form.Input 
+								name='location'
+								type='text'
+								value={this.state.value}
+								placeholder='location :)'
+								onChange={this.handleChange}
+							/>
+							<Label> bio </Label>
+							<Form.TextArea 
+								name='bio'
+								type='text'
+								value={this.state.value}
+								placeholder='bio :)'
+								onChange={this.handleChange}
+							/>
+						</React.Fragment>
+					}
+					<Button type='submit'> {this.state.action} </Button>
 				</Form>
+				{
+					this.state.action == "Login"
+					?
+					<p> Don't have an account? <span onClick={this.switchAction} className='fake-link'>Make one here :)</span></p>
+					:
+					<p> Already have an account? <span onClick={this.switchAction} className='fake-link'>Sign in here :)</span></p>
+				}
 			</div>
 		)
 	}
